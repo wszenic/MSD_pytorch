bead9d944f15f3405e40fe0b78e50b3d3b109eac
Wojciech Szenic <w.szenic@amainstitute.pl>
2020-09-15 21:02:35 +0200

    added helpers

---
+++ helper/preprocess.py
@@ -0,0 +1,85 @@
+<<<<<<< HEAD
+from MSD.settings.config import LARGE_DATASET, PREPROCESS_FOLDER, DATASET_DESCRIPTION
+from PIL import Image
+from tqdm import tqdm
+import glob
+from torchvision import transforms
+import torch
+import pandas as pd
+
+
+class Cropper:
+
+    def __init__(self):
+        self.path = LARGE_DATASET
+
+        self.transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.558926, 0.42738813, 0.4077543),
+                                 std=(0.24672212, 0.236501, 0.22921552))
+        ])
+
+    def crop_photos(self):
+        labels_df = pd.read_csv(DATASET_DESCRIPTION)
+        labels_df = labels_df[['file_names', 'channels']]
+        labels_df = labels_df.set_index('file_names')
+
+        labels_dict = labels_df.to_dict('index')
+
+        for file in tqdm(glob.glob(LARGE_DATASET + '/*.jpg')):
+            file_name = file.split('/')[-1]
+
+            if labels_dict[file_name]['channels'] == 3.0:
+                img = Image.open(file)
+                out_tensor = self.transforms(img)
+                torch.save(out_tensor, PREPROCESS_FOLDER + '/' + file_name.split('.')[0] + '.pt')
+
+
+
+cropper = Cropper()
+cropper.crop_photos()
+=======
+from MSD.settings.config import LARGE_DATASET, PREPROCESS_FOLDER, DATASET_DESCRIPTION
+from PIL import Image
+from tqdm import tqdm
+import glob
+from torchvision import transforms
+import torch
+import pandas as pd
+
+
+class Cropper:
+
+    def __init__(self):
+        self.path = LARGE_DATASET
+
+        self.transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.558926, 0.42738813, 0.4077543),
+                                 std=(0.24672212, 0.236501, 0.22921552))
+        ])
+
+    def crop_photos(self):
+        labels_df = pd.read_csv(DATASET_DESCRIPTION)
+        labels_df = labels_df[['file_names', 'channels']]
+        labels_df = labels_df.set_index('file_names')
+
+        labels_dict = labels_df.to_dict('index')
+
+        for file in tqdm(glob.glob(LARGE_DATASET + '/*.jpg')):
+            file_name = file.split('/')[-1]
+
+            if labels_dict[file_name]['channels'] == 3.0:
+                img = Image.open(file)
+                out_tensor = self.transforms(img)
+                torch.save(out_tensor, PREPROCESS_FOLDER + '/' + file_name.split('.')[0] + '.pt')
+
+
+
+cropper = Cropper()
+cropper.crop_photos()
+>>>>>>> f0477cfe305cbd62361a4f08bd409ee4d6329c2a
---
+++ helper/train_test_split.py
@@ -0,0 +1,71 @@
+<<<<<<< HEAD
+from MSD.settings.config import LABEL_FILE_TRAIN, LARGE_DATASET, DATASET_DESCRIPTION
+import pandas as pd
+import numpy as np
+from scipy.io import loadmat
+from PIL import Image
+import os
+
+def create_description_matrix():
+    label_matrix = loadmat(LABEL_FILE_TRAIN)['annotations'].reshape(-1,1)
+
+    file_names = [label_matrix[x][0][0][0].split('/')[1] for x in range(len(label_matrix))]
+    img_classes = [label_matrix[x][0][5][0][0] for x in range(len(label_matrix))]
+    is_test = [label_matrix[x][0][6][0][0] for x in range(len(label_matrix))]
+
+    shapes = []
+    for i in range(len(label_matrix)):
+        img = Image.open(LARGE_DATASET + '/' + file_names[i])
+        arr = np.array(img)
+        shapes.append(arr.shape)
+
+    shapes_df = pd.DataFrame(shapes, columns=['height', 'width', 'channels'])
+
+    names_df = pd.DataFrame({'file_names':  file_names,
+                             'class_id':    img_classes,
+                             'is_test':     is_test})
+
+    names_df['full_path'] = LARGE_DATASET + '/' + names_df.file_names
+
+    names_df = pd.concat([names_df, shapes_df], axis='columns')
+
+    names_df.to_csv(DATASET_DESCRIPTION, index=False)
+
+create_description_matrix()
+
+=======
+from MSD.settings.config import LABEL_FILE_TRAIN, LARGE_DATASET, DATASET_DESCRIPTION
+import pandas as pd
+import numpy as np
+from scipy.io import loadmat
+from PIL import Image
+import os
+
+def create_description_matrix():
+    label_matrix = loadmat(LABEL_FILE_TRAIN)['annotations'].reshape(-1,1)
+
+    file_names = [label_matrix[x][0][0][0].split('/')[1] for x in range(len(label_matrix))]
+    img_classes = [label_matrix[x][0][5][0][0] for x in range(len(label_matrix))]
+    is_test = [label_matrix[x][0][6][0][0] for x in range(len(label_matrix))]
+
+    shapes = []
+    for i in range(len(label_matrix)):
+        img = Image.open(LARGE_DATASET + '/' + file_names[i])
+        arr = np.array(img)
+        shapes.append(arr.shape)
+
+    shapes_df = pd.DataFrame(shapes, columns=['height', 'width', 'channels'])
+
+    names_df = pd.DataFrame({'file_names':  file_names,
+                             'class_id':    img_classes,
+                             'is_test':     is_test})
+
+    names_df['full_path'] = LARGE_DATASET + '/' + names_df.file_names
+
+    names_df = pd.concat([names_df, shapes_df], axis='columns')
+
+    names_df.to_csv(DATASET_DESCRIPTION, index=False)
+
+create_description_matrix()
+
+>>>>>>> f0477cfe305cbd62361a4f08bd409ee4d6329c2a
