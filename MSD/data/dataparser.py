f0477cfe305cbd62361a4f08bd409ee4d6329c2a
wszenic <62420868+wszenic@users.noreply.github.com>
2020-09-15 19:47:26 +0200

    Update dataparser.py

--- MSD/data/dataparser.py
+++ MSD/data/dataparser.py
@@ -1,7 +1,7 @@
-from MSD.settings.config import LABEL_FILE_TRAIN, TRAIN_FOLDER, DATASET_DESCRIPTION
+from MSD.settings.config import LABEL_FILE_TRAIN, TRAIN_FOLDER, DATASET_DESCRIPTION,\
+                                IMAGE_COLOUR_MODE
 
 import pandas as pd
-from torchvision import transforms
 from scipy.io import loadmat
 from PIL import Image
 import torch
@@ -9,28 +9,23 @@ import torch
 
 class CarsDataset:
 
-    def __init__(self, path_list, use_transforms=True):
-        self.use_transforms = use_transforms
+    def __init__(self, path_list, transforms):
+        self.transforms = transforms
         self.image_paths = path_list
 
         labels_df = pd.read_csv(DATASET_DESCRIPTION)
-        labels_df = labels_df[['preprocess_path', 'class_id']]
-        labels_df = labels_df.set_index('preprocess_path')
+        labels_df = labels_df[['full_path', 'class_id']]
+        labels_df = labels_df.set_index('full_path')
         label_dict = labels_df.to_dict('index')
 
-        self.labels = [label_dict[x]['class_id'] for x in self.image_paths]
-
-        self.transforms = transforms.Compose([
-            transforms.RandomHorizontalFlip(0.5),
-        ])
+        self.labels = [label_dict[x]['class_id'] - 1 for x in self.image_paths]
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, item):
-        image_tensor = torch.load(self.image_paths[item])
-        if self.use_transforms:
-            image_tensor = self.transforms(image_tensor)
+        img = Image.open(self.image_paths[item]).convert(IMAGE_COLOUR_MODE)
+        image_tensor = self.transforms(img)
         label = torch.tensor(self.labels[item], dtype=torch.long)
 
         return image_tensor, label
