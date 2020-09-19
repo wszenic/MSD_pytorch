72d2be51f5bd101921ee892af80cf0e23a7f02c0
Wojciech Szenic <w.szenic@amainstitute.pl>
2020-09-19 14:29:48 +0200

    Corrected forward method to account for recursive concatenations

--- MSD/models/MSD_net.py
+++ MSD/models/MSD_net.py
@@ -15,97 +15,123 @@ class MSDnet(nn.Module):
         )
 
         # layer 1
-        self.h11_stride = Layer1Scale(in_ch=3,
-                                      out_ch=SCALE_CHANNELS['scale_1'], stride=2, pad=1)
-        self.h21_stride = Layer1Scale(in_ch=SCALE_CHANNELS['scale_1'],
-                                      out_ch=SCALE_CHANNELS['scale_2'], stride=2, pad=1)
-
         self.h11_regular = Layer1Scale(in_ch=3,
                                        out_ch=SCALE_CHANNELS['scale_1'], stride=1, pad=1)
-        self.h21_regular = Layer1Scale(in_ch=SCALE_CHANNELS['scale_1'],
-                                       out_ch=SCALE_CHANNELS['scale_2'], stride=1, pad=1)
-        self.h31_regular = Layer1Scale(in_ch=SCALE_CHANNELS['scale_2'],
-                                       out_ch=SCALE_CHANNELS['scale_3'], stride=1, pad=1)
+
+        self.h21_stride = Layer1Scale(in_ch=SCALE_CHANNELS['scale_1'],
+                                      out_ch=SCALE_CHANNELS['scale_2'], stride=2, pad=2)
+
+        self.h31_stride = Layer1Scale(in_ch=SCALE_CHANNELS['scale_2'],
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=2, pad=2)
 
         # layer 2
-        self.h12_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_1'],
-                                      mid_ch=SCALE_CHANNELS['scale_1'],
-                                      out_ch=SCALE_CHANNELS['scale_1'], stride=2, pad=1)
-        self.h22_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_1'] + SCALE_CHANNELS['scale_2'],
+        self.h12_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_1'],
+                                       mid_ch=SCALE_CHANNELS['scale_1'],
+                                       out_ch=SCALE_CHANNELS['scale_1'], stride=1, pad=1)
+
+        self.h22_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_1'],
                                       mid_ch=SCALE_CHANNELS['scale_2'],
                                       out_ch=SCALE_CHANNELS['scale_2'], stride=2, pad=2)
-
-        
-        self.h22_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_1'] + SCALE_CHANNELS['scale_2'],
+        self.h22_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_2'],
                                        mid_ch=SCALE_CHANNELS['scale_2'],
                                        out_ch=SCALE_CHANNELS['scale_2'], stride=1, pad=1)
-        self.h32_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_2'] + SCALE_CHANNELS['scale_3'],
+
+        self.h32_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_2'],
+                                      mid_ch=SCALE_CHANNELS['scale_3'],
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=2, pad=2)
+        self.h32_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_3'],
                                        mid_ch=SCALE_CHANNELS['scale_3'],
                                        out_ch=SCALE_CHANNELS['scale_3'], stride=1, pad=1)
-
-
         # layer 3
-        self.h23_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_2'] * 2,
+        self.h23_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_1']*2,
                                       mid_ch=SCALE_CHANNELS['scale_2'],
                                       out_ch=SCALE_CHANNELS['scale_2'], stride=2, pad=2)
+        self.h23_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_2']*2,
+                                      mid_ch=SCALE_CHANNELS['scale_2'],
+                                      out_ch=SCALE_CHANNELS['scale_2'], stride=1, pad=1)
 
-        self.h33_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_3'] * 2,
+        self.h33_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_2']*2,
                                       mid_ch=SCALE_CHANNELS['scale_3'],
-                                      out_ch=SCALE_CHANNELS['scale_3'], stride=1, pad=2)
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=2, pad=2)
+        self.h33_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_3']*2,
+                                      mid_ch=SCALE_CHANNELS['scale_3'],
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=1, pad=1)
+
+        # layer 4
+        self.h34_stride = LayerNScale(in_ch=SCALE_CHANNELS['scale_2']*3,
+                                      mid_ch=SCALE_CHANNELS['scale_3'],
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=2, pad=2)
+
+        self.h34_regular = LayerNScale(in_ch=SCALE_CHANNELS['scale_3']*3,
+                                      mid_ch=SCALE_CHANNELS['scale_3'],
+                                      out_ch=SCALE_CHANNELS['scale_3'], stride=1, pad=1)
 
 
         # classifiers
-        self.classifier_l2 = Classifier(in_ch=SCALE_CHANNELS['scale_3'] + SCALE_CHANNELS['scale_2'],
-                                        mid_ch=SCALE_CHANNELS['scale_3'] + SCALE_CHANNELS['scale_2'],
-                                        out_ch=SCALE_CHANNELS['scale_3'] + SCALE_CHANNELS['scale_2'],
+        self.classifier_l2 = Classifier(in_ch=SCALE_CHANNELS['scale_3']*2,
+                                        mid_ch=128,
+                                        out_ch=128,
                                         in_shape=3)
 
-        self.classifier_l3 = Classifier(in_ch= SCALE_CHANNELS['scale_3']*2,
-                                        mid_ch= SCALE_CHANNELS['scale_3']*2,
-                                        out_ch= SCALE_CHANNELS['scale_3']*2, in_shape=3)
+        self.classifier_l3 = Classifier(in_ch=SCALE_CHANNELS['scale_3']*4,
+                                        mid_ch=128,
+                                        out_ch=128, in_shape=3)
 
-        self.classifier_l4 = Classifier(in_ch= SCALE_CHANNELS['scale_3']*3,
-                                        mid_ch= SCALE_CHANNELS['scale_3']*3,
-                                        out_ch= SCALE_CHANNELS['scale_3']*3, in_shape=3)
+        self.classifier_l4 = Classifier(in_ch=SCALE_CHANNELS['scale_3']*6,
+                                        mid_ch=128,
+                                        out_ch=128, in_shape=3)
+
 
     def forward(self, x):
         x_reduced = self.initial_reduce(x)
-        
+
         # layer 1
-        h11_s_out = self.h11_stride(x_reduced)
+        x_1_1 = self.h11_regular(x_reduced)
 
-        h11_r_out = self.h11_regular(x_reduced)
-        h21_s_out = self.h21_stride(h11_s_out)
+        x_2_1 = self.h21_stride(x_1_1)
 
-        h21_r_out = self.h21_regular(h11_s_out)
-        h31_r_out = self.h31_regular(h21_s_out)
+        x_3_1 = self.h31_stride(x_2_1)
 
         # layer 2
-        h12_s_out = self.h12_stride(h11_r_out)
+        x_1_2 = self.h12_regular(x_1_1)
 
-        h22_in = torch.cat((h11_s_out, h21_r_out), dim=1)
-        h22_s_out = self.h22_stride(h22_in)
-        h22_r_out = self.h22_regular(h22_in)
+        x_2_2 = torch.cat((
+            self.h22_stride(x_1_1),
+            self.h22_regular(x_2_1)
+            ), dim=1
+        )
 
-        h32_in = torch.cat((h21_s_out, h31_r_out), dim=1)
-        h32_r_out = self.h32_regular(h32_in)
+        x_3_2 = torch.cat((
+            self.h32_stride(x_2_1),
+            self.h32_regular(x_3_1)
+            ), dim=1
+        )
 
         # layer 3
-        h23_in = torch.cat((h21_r_out, h22_r_out), dim=1)
-        h23_s_out = self.h23_stride(h23_in)
+        x_2_3 = torch.cat((
+            self.h23_stride(torch.cat((x_1_1, x_1_2), dim=1)),
+            self.h23_regular(torch.cat((x_2_1, x_2_2), dim=1))
+            ), dim=1
+        )
 
-        h33_in = torch.cat((h31_r_out, h32_r_out), dim=1)
-        h33_r_out = self.h33_regular(h33_in)
+        x_3_3 = torch.cat((
+            self.h33_stride(torch.cat((x_2_1, x_2_2), dim=1)),
+            self.h33_regular(torch.cat((x_3_1, x_3_2), dim=1))
+            ), dim=1
+        )
 
-        #layer 4 <- virtual, as only serves as classifier input
-        h41_in = torch.cat((h33_in, h33_r_out), dim=1)
+        # layer 4
+        x_3_4 = torch.cat((
+            self.h34_stride(torch.cat((x_2_1, x_2_2, x_2_3), dim=1)),
+            self.h34_regular(torch.cat((x_3_1, x_3_2, x_3_3), dim=1))
+            ), dim=1
+        )
 
-        #classifiers
-        classifier_layer_2 = self.classifier_l2(h32_in)
+        # classifiers
 
-        classifier_layer_3 = self.classifier_l3(h33_in)
-
-        classifier_layer_4 = self.classifier_l4(h41_in)
+        classifier_layer_2 = self.classifier_l2(x_3_2)
+        classifier_layer_3 = self.classifier_l3(x_3_3)
+        classifier_layer_4 = self.classifier_l4(x_3_4)
 
         return classifier_layer_2, classifier_layer_3, classifier_layer_4
 
@@ -156,8 +182,8 @@ class Classifier(nn.Module):
     def __init__(self, in_ch, mid_ch, out_ch, in_shape):
         super().__init__()
 
-        self.classifier_conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3)
-        self.classifier_conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=3)
+        self.classifier_conv1 = nn.Conv2d(in_ch, mid_ch, kernel_size=3, padding=0)
+        self.classifier_conv2 = nn.Conv2d(mid_ch, out_ch, kernel_size=3, padding=0)
         self.classifier_avgpool = nn.AvgPool2d(3)
 
         self.dense = nn.Linear(out_ch * in_shape**2,
